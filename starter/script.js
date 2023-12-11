c790c0a79ce779e93fe2a1d35ab4d979aea3f8a8
kenzo7664 <macbook@Emmanuels-MBP.home>
2023-12-11 13:47:13 +0000

    Refractored the getDate function

--- starter/script.js
+++ starter/script.js
@@ -22,8 +22,8 @@ const account1 = {
     '2023-04-01T10:17:24.185Z',
     '2023-05-08T14:11:59.604Z',
     '2023-05-27T17:01:17.194Z',
-    '2023-07-11T23:36:17.929Z',
-    '2023-07-12T10:51:36.790Z',
+    '2023-12-07T23:36:17.929Z',
+    '2023-12-09T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -80,6 +80,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1)
+    / (1000 * 60 * 60 * 24))
+
+  const daysPassed = calcDaysPassed(new Date(), date)
+  if (daysPassed === 0) return 'Today'
+  if (daysPassed === 1) return "Yesterday"
+  if (daysPassed <= 7) return `${daysPassed} days ago`
+  else {
+    const day = `${date.getDate()}`.padStart(2, "0")
+    const month = `${date.getMonth() + 1}`.padStart(2, "0")
+    const year = date.getFullYear()
+
+    return `${day}/${month}/${year}`
+  }
+}
 
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -89,10 +105,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(acc.movementsDates[i])
-    const day = `${date.getDate()}`.padStart(2, "0")
-    const month = `${date.getMonth() + 1}`.padStart(2, "0")
-    const year = date.getFullYear()
-    const displayDate = `${day}/${month}/${year}`
+    const displayDate = formatMovementDate(date)
 
     const html = `
       <div class="movements__row">
@@ -269,12 +282,3 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
-// Lectures
-// Operation with Dates 
-const future = new Date(2037, 10, 19, 15, 23)
-console.log(+(future));
-
-const daysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24)
-
-const days = daysPassed(new Date(2037, 10, 30), new Date(2037, 10, 24))
-console.log(days);
