1e7a25ef99626b6925f8b80782bc06ab5e2c1a1b
kenzo7664 <macbook@Emmanuels-MBP.home>
2023-12-12 19:47:06 +0000

    setTimeout

--- starter/script.js
+++ starter/script.js
@@ -297,3 +297,8 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
+// setTimeout
+const ingredients = ["Tomato", "cherry"]
+const pizzaTimer = setTimeout((ing1, ing2) =>
+  console.log(`Here is your Pizza with ${ing1} and ${ing2}`), 3000, ...ingredients)
+if (ingredients.includes("cherry")) clearTimeout(pizzaTimer)
