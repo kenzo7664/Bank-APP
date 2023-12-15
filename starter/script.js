470baf911b3b336c82690614ba3cb3f7384bd708
Emmanuel Olaotan <emmanuel@Emmanuels-MBP-2.home>
2023-12-15 12:00:38 +0000

    Timer function to Log out due to inactivity

--- starter/script.js
+++ starter/script.js
@@ -179,9 +179,37 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function(){
+  const tick = function(){
+    const min = String(Math.trunc(time/60)).padStart(2,0)
+    const sec = String(time % 60).padStart(2, 0)
+
+    //In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`
+
+    //When 0 seconds, stop timer and log out user
+    if (time === 0){
+      clearInterval(timer);
+      labelWelcome.textContent = "Log in to get started"
+      containerApp.style.opacity = 0
+    }
+
+    //Decrease 1s
+    time-- ;
+  }
+
+  // Set time to 5 minutes 
+  let time = 150;
+
+  // Call the timer evrry second
+  tick()
+  const timer = setInterval(tick, 1000)
+
+  return timer 
+}
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 
 // Handling Login
@@ -219,6 +247,11 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    // Timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -248,6 +281,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 // Handling Loan Transactions
@@ -264,6 +301,10 @@ btnLoan.addEventListener('click', function (e) {
       currentAccount.movementsDates.push(new Date().toISOString())
       // Update UI
       updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 3000)
   }
   inputLoanAmount.value = '';
